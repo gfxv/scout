5cbf7d9f5ac607b256133faf7cf06e809d13ba55
gfxv <superfreezu@gmail.com>
2025-01-31 13:25:42 +0300

    feat: add stemming to tokenizer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gfxv/scout
 
 go 1.23.3
+
+require github.com/kljensen/snowball v0.10.0 // indirect
--- tokenizer.go
+++ tokenizer.go
@@ -3,6 +3,8 @@ package main
 import (
 	"strings"
 	"unicode"
+
+	"github.com/kljensen/snowball"
 )
 
 type Tokenizer struct {
@@ -13,6 +15,30 @@ func NewTokenizer(data []rune) *Tokenizer {
 	return &Tokenizer{
 		data: data,
 	}
+}
+
+func (t *Tokenizer) NextToken() (string, bool) {
+	t.trimLeftSpaces()
+	if len(t.data) == 0 {
+		return "", false
+	}
+
+	if unicode.IsDigit(t.data[0]) {
+		token := t.removeIf(func(r rune) bool {
+			return unicode.IsDigit(r)
+		})
+		return string(token), true
+	}
+
+	if unicode.IsLetter(t.data[0]) {
+		token := t.removeIf(func(r rune) bool {
+			return unicode.IsLetter(r) || unicode.IsDigit(r)
+		})
+		stemmed := t.stem(strings.ToLower(string(token)))
+		return stemmed, true
+	}
+
+	return string(t.removeFirst(1)), true
 }
 
 func (t *Tokenizer) trimLeftSpaces() {
@@ -39,25 +65,11 @@ func (t *Tokenizer) removeIf(condition func(rune) bool) []rune {
 	return t.removeFirst(n)
 }
 
-func (t *Tokenizer) NextToken() (string, bool) {
-	t.trimLeftSpaces()
-	if len(t.data) == 0 {
-		return "", false
-	}
-
-	if unicode.IsDigit(t.data[0]) {
-		token := t.removeIf(func(r rune) bool {
-			return unicode.IsDigit(r)
-		})
-		return string(token), true
-	}
-
-	if unicode.IsLetter(t.data[0]) {
-		token := t.removeIf(func(r rune) bool {
-			return unicode.IsLetter(r) || unicode.IsDigit(r)
-		})
-		return strings.ToLower(string(token)), true
-	}
-
-	return string(t.removeFirst(1)), true
+func (t *Tokenizer) stem(token string) string {
+	// last boolean argument reduces all the 'useless' words from the language
+	// like which, and, why, with, but, etc. (acutally doesn't really matter,
+	// because tf-idf handles these words quite good).
+	// error may happen only if the language provided is unknown for the library
+	stemmed, _ := snowball.Stem(token, "english", true)
+	return stemmed
 }
